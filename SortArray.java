92b9a952c0644776bda4ee3aff601632a84aa485
ganesh.mane <2023bit501@sggs.ac.in>
2025-08-15 20:23:29 +0530

    Sort Array Day 1

---
+++ SortArray.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class SortArray{
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int zeroCount=0;
+        int oneCount=0;
+        int twoCount=0;
+        for(int data : arr){
+            if(data == 0){
+                zeroCount++;
+            }else if(data == 1){
+                oneCount++;
+            }else{
+                twoCount++;
+            }
+        }
+        
+        int i=0;
+        while(zeroCount != 0){
+            arr[i] = 0;
+            zeroCount--;
+            i++;
+        }
+        while(oneCount != 0){
+            arr[i] = 1;
+            oneCount--;
+            i++;
+        }
+        while(twoCount != 0){
+            arr[i] = 2;
+            twoCount--;
+            i++;
+        }
+
+        for(int data : arr){
+            System.out.print(data+" ");
+        }
+    }
+}
